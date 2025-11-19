91a7075a0238ae9b0b5af8bf39d13646ca0733fb
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-11-19 19:01:34 +0500

    depth

--- backend/app/engine/stockfish.py
+++ backend/app/engine/stockfish.py
@@ -39,7 +39,7 @@ class StockfishEngine:
         try:
             self.engine = Stockfish(
                 path=self.path,
-                depth=depth,
+                depth=self.depth,
                 parameters={
                     "Threads": self.threads,
                     "Hash": self.hash_mb,
