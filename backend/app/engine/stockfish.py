48f7887727603c31211b85022df1fe896751a4f0
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-11-19 17:16:05 +0500

    path

--- backend/app/engine/stockfish.py
+++ backend/app/engine/stockfish.py
@@ -12,7 +12,7 @@ class StockfishEngine:
     
     def __init__(
         self,
-        path: Optional[str] = None,
+        path: Optional[str] = r"C:\Users\Muhammad Fezan\Downloads\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe",
         depth: Optional[int] = 5,
         threads: Optional[int] = 4,
         hash_mb: Optional[int] = 256,
